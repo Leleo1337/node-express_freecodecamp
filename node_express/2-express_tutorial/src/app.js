e4a09789890d30d58094f3773886b631a38e4bfc
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-05-05 17:59:40 -0300

    Update app.js

--- node_express/2-express_tutorial/src/app.js
+++ node_express/2-express_tutorial/src/app.js
@@ -1,1 +1,15 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer((req,res) => {
+    res.writeHead(200, {"content-type": 'text/html'})
+    if(req.url === '/'){
+        res.write('<h1> Home page </h1>')
+        res.end()
+    }else if((req.url === '/contact')){
+        res.write('<h1> Contact page </h1>')
+        res.end()
+    }else{
+        res.end('<h1> something went wrong! go back to <a href="/">Home</a></h1>')
+    }
+})
+server.listen(5000, () => console.log('server running on port: 5000'))
