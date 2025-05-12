b9a6e051db51762cb7909991e3b03243469a324b
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-05-12 16:28:25 -0300

    fixing folders

--- rest_Wild _horizons/utils/getDataByPathParams.js
+++
@@ -1,7 +0,0 @@
-export function getDataByPathParams(destination, locationType, locationName) {
-   const filteredData = destination.filter((data) => {
-      return data[locationType].toLowerCase() === locationName.toLowerCase();
-   });
-
-   return filteredData;
-}
--- rest_Wild _horizons/utils/sendJSONresponse.js
+++
@@ -1,7 +0,0 @@
-export function sendJSONresponse(res, status, end){
-    res.setHeader('Content-Type', 'application/json')
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET')
-    res.statusCode = status
-    res.end(JSON.stringify(end))
-}
